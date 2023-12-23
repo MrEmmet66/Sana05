308f9e40d60dd80bbf459d658450f32d4186c2b6
MrEmmet66 <lorrenc7@gmail.com>
2023-12-23 14:26:08 +0200

    Add constructors

--- Models/Currency.cs
+++ Models/Currency.cs
@@ -8,6 +8,18 @@ namespace Sana05.Models
 {
     public class Currency
     {
+        public Currency(string name, decimal exRate)
+        {
+            this.name = name;
+            this.exRate = exRate;
+        }
+        public Currency(string name)
+        {
+            this.name = name;
+        }
+        public Currency(Currency currency) : this(currency.name, currency.exRate) { }
+        public Currency() { }
+
         protected string name;
         protected decimal exRate;
     }
