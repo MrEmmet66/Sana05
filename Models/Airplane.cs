c2c75ab892de712133307f046c81d70f4bea0eb8
MrEmmet66 <lorrenc7@gmail.com>
2023-12-23 14:07:40 +0200

    Added class fields

--- Models/Airplane.cs
+++ Models/Airplane.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sana05
+namespace Sana05.Models
 {
     public class Airplane
     {
+        protected string startCity;
+        protected string finishCity;
+        protected MyDate startDate;
+        protected MyDate finishDate;
     }
 }
