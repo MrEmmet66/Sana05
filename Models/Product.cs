83cd1004a58b03d3c5fb94f7f585f9ff75939fa4
MrEmmet66 <lorrenc7@gmail.com>
2023-12-23 14:20:16 +0200

    Add fields

--- Models/Product.cs
+++ Models/Product.cs
@@ -8,5 +8,11 @@ namespace Sana05.Models
 {
     public class Product
     {
+        protected string name;
+        protected decimal price;
+        protected Currency cost;
+        protected int quantity;
+        protected string producer;
+        protected float weight;
     }
 }
