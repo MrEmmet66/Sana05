cfe1711454ba3c1a929dfcb199bbaf3dcac95e64
MrEmmet66 <lorrenc7@gmail.com>
2023-12-23 14:09:52 +0200

    Add constructors to class

--- Models/Airplane.cs
+++ Models/Airplane.cs
@@ -8,9 +8,28 @@ namespace Sana05.Models
 {
     public class Airplane
     {
+        public Airplane(string startCity, string finishCity, MyDate startDate, MyDate finishDate)
+        {
+            this.startCity = startCity;
+            this.finishCity = finishCity;
+            this.startDate = startDate;
+            this.finishDate = finishDate;
+        }
+        public Airplane(string startCity, string finishCity, MyDate startDate)
+        {
+            this.startCity = startCity;
+            this.finishCity = finishCity;
+            this.startDate = startDate;
+        }
+        public Airplane(Airplane airplane) : this(airplane.startCity, airplane.finishCity, airplane.startDate, airplane.finishDate) { }
+        public Airplane() { }
+
+
         protected string startCity;
         protected string finishCity;
         protected MyDate startDate;
         protected MyDate finishDate;
+
+
     }
 }
